c9fd202505f8f93f72234419cf46349ba6633351
JBNRZ <3537659915@qq.com>
2023-10-10 21:35:20 +0800

    Init REPO

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# 0RAYS-Bot
+基于Nonebot2的QQ机器人
+
+# 开发文档
+go-cqhttp: https://docs.go-cqhttp.org/
+
+nonebot: https://nonebot.dev/
---
+++ bot.py
@@ -0,0 +1,14 @@
+import nonebot
+from nonebot.adapters.onebot.v11 import Adapter as V11Adapter
+
+
+nonebot.init()
+driver = nonebot.get_driver()
+driver.register_adapter(V11Adapter)
+
+nonebot.load_builtin_plugin("echo")
+nonebot.load_plugins("plugins/sole")
+
+
+if __name__ == "__main__":
+    nonebot.run()
