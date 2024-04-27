562095ebf2715698a80d11a9a013044044700040
JBNRZ <3537659915@qq.com>
2024-04-28 01:10:14 +0800

    Fixed path and some bugs

--- bot.py
+++ bot.py
@@ -1,14 +1,13 @@
 import nonebot
 from nonebot.adapters.onebot.v11 import Adapter as V11Adapter
 
-
 nonebot.init()
 driver = nonebot.get_driver()
 driver.register_adapter(V11Adapter)
 
 nonebot.load_builtin_plugin("echo")
-nonebot.load_plugins("plugins/gif")
 nonebot.load_plugins("plugins/single")
+nonebot.load_plugins("plugins/welcome")
 
 
 if __name__ == "__main__":
