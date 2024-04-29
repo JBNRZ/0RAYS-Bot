f9732be4ad9813fd7aa8f296597f3069c84f1520
JBNRZ <3537659915@qq.com>
2024-04-30 02:16:18 +0800

    Tidy plugins

--- bot.py
+++ bot.py
@@ -6,9 +6,12 @@ driver = nonebot.get_driver()
 driver.register_adapter(V11Adapter)
 
 nonebot.load_builtin_plugin("echo")
-nonebot.load_plugins("plugins/single")
-nonebot.load_plugins("plugins/welcome")
+nonebot.load_plugins("plugins")
 
 
 if __name__ == "__main__":
     nonebot.run()
+
+
+
+
--- plugins/welcome/__init__.py
+++ plugins/welcome/__init__.py
@@ -1,17 +1,34 @@
+import smtplib
+from email.header import Header
+from email.mime.text import MIMEText
 from uuid import uuid4
 
 from Crypto.Cipher import AES
-from nonebot import get_driver, on_command
-from nonebot.adapters.onebot.v11 import Bot, NoticeEvent, GroupIncreaseNoticeEvent
-from nonebot.adapters.onebot.v11 import Message, MessageSegment
-from nonebot.adapters.onebot.v11 import GroupMessageEvent
-from nonebot.log import logger
+from nonebot import on_command, logger, get_driver, get_bot
+from nonebot.adapters.onebot.v11 import NoticeEvent, Bot, Message, MessageSegment, GroupMessageEvent, \
+    GroupIncreaseNoticeEvent
 from nonebot.permission import SUPERUSER
 from nonebot.plugin import on_notice
 from nonebot.rule import Rule, to_me
-from requests import post
+from nonebot_plugin_apscheduler import scheduler
+from requests import get, post
 
-from ._send_email import send_email
+
+def send_email(qq: str, msg: str):
+    sender = get_driver().config.oauth_email_sender
+    receivers = [f'{qq}@qq.com']
+
+    mail_host = get_driver().config.oauth_email_host
+    mail_pass = get_driver().config.oauth_email_pwd
+
+    message = MIMEText(msg, 'plain', 'utf-8')
+    message['From'] = Header(f"{sender.split('@')[0]} <{sender}>")
+    message['To'] = Header(f"{qq}@qq.com", 'utf-8')
+    message['Subject'] = Header("杭电身份认证", 'utf-8')
+    s = smtplib.SMTP()
+    s.connect(mail_host, get_driver().config.oauth_email_port)
+    s.login(sender, mail_pass)
+    s.sendmail(sender, receivers, message.as_string())
 
 
 def padding(msg: bytes):
@@ -118,3 +135,51 @@ async def resend(bot: Bot, event: GroupMessageEvent):
             user_id=event.user_id,
             message=Message(f"{qq} 身份验证出现了点儿小问题: {e}")
         )
+
+
+@scheduler.scheduled_job("cron", hour="0", id="Kick")
+async def kick_unchecked_user():
+    url = get_driver().config.oauth_server.strip("/") + "/oauth/kick"
+    cookie = {
+        "reg-code": get_driver().config.oauth_register_code
+    }
+    response = get(url, cookies=cookie)
+    if response.status_code == 200 and response.json()["code"] == 0:
+        bot: Bot = get_bot()
+        for baned in response.json()["data"]:
+            qq, group = baned.split("-")
+            send_email(qq, "未及时进行身份认证，可再次申请加群进行验证，如未收到验证邮箱，请检查QQ邮箱垃圾箱")
+            logger.info(f"Send kick email to {qq} in {group}")
+            await bot.set_group_kick(group_id=int(group), user_id=qq)
+            logger.info(f"Kick {qq} from {group}")
+
+
+@scheduler.scheduled_job("cron", second="*", id="Unban")
+async def check_baned_user():
+    url = get_driver().config.oauth_server.strip("/") + "/oauth/unban"
+    cookie = {
+        "reg-code": get_driver().config.oauth_register_code
+    }
+    response = get(url, cookies=cookie)
+    if response.status_code == 200 and response.json()["code"] == 0:
+        bot: Bot = get_bot()
+        for baned in response.json()["data"]:
+            qq, group = baned.split("-")
+            await bot.set_group_ban(group_id=int(group), user_id=int(qq), duration=0)
+            msg = Message(MessageSegment.at(qq))
+            msg += "欢迎参加第七届赛博杯，祝你在这里玩的愉快，有问题找管理\n平台地址: https://training.0rays.club"
+            # msg += f"欢迎来到 0RAYS 2023 招新群！！！" + Message(MessageSegment.face('99')) + Message(
+            #     MessageSegment.face('2')) + "\n"
+            # msg += "你可能还不了解CTF是什么，巧了，我也不知道，我只是个机器人\n"
+            # msg += "但这都不是问题，计算机方面最不缺的就是教学资源了（善用百度谷歌等搜索引擎）\n"
+            # msg += "1. 去ctf wiki看看ctf到底是怎么回事吧\nhttps://ctf-wiki.org/（在这里你将能了解CTF的各个方向）\n"
+            # msg += "2. 选择一个你喜欢的方向，做几道货真价实的CTF题目体会体会\n看看ctfhub的技能树\n"
+            # msg += "https://www.ctfhub.com/#/index（一个很好的CTF学习网站，里面的赛事中心会公布各大国内外赛事的信息，嘛嘛再也不用担心我找不到比赛了）\n"
+            # msg += "去攻防世界做几道新手入门题\nhttps://adworld.xctf.org.cn（这里汇集了大量的CTF真题，并且非常人性化的设置了新手区和进阶区，助你登堂入室）\n"
+            # msg += f"啊对，还有0RAYS自己的平台：https://training.0rays.club/（打不开就杀运维" + Message(
+            #     MessageSegment.at(manager)) + "\n"
+            # msg += "还有bugku https://ctf.bugku.com/ 等等\n"
+            # msg += "实在不行@群里的管理员，他们会乐意回答一些入门问题"
+            # msg += f"管理员不在线怎么办，那就私聊" + Message(MessageSegment.at(manager)) + "（这哥们几乎啥时候都在线\n"
+            # msg += "最后的最后，放出一个重磅的消息：一年一度的赛博杯马上就要来啦！！(预计将在10月份举办)届时我们也将第一次招纳新成员，期待优秀的你能够加入我们~\n"
+            await bot.send_group_msg(group_id=group, message=msg)
